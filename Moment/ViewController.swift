5f7f1081b31108341c85c9ca96f06157c7613026
Alessandro Losavio <alessandro.losavio@mail.polimi.it>
2019-03-18 00:33:13 +0100

    Initial Commit

---
+++ Moment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Moment
+//
+//  Created by Alessandro Losavio on 18/03/2019.
+//  Copyright © 2019 Losavio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
